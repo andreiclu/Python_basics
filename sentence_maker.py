bc9359b9a8f25956b90508b8a60c8cc1f88c429b
Andrei Clucerescu <adyac92@gmail.com>
2021-09-23 14:26:47 +0300

    basics_modules

--- sentence_maker.py
+++ sentence_maker.py
@@ -6,4 +6,17 @@ def sentence_maker(phrase):
     else:
         return "{}.".format(capitalized)
 
-print(sentence_maker("how are you"))      
+#print(sentence_maker("how are you"))
+
+results = []#create an empty list 
+while True:
+    user_input = input ("Say something: ") #ask user input
+    if user_input == "\end": #check if the input is finished
+        break
+    else:
+        results.append(sentence_maker(user_input)) #append the user input in the results list we created it, useing the function by calling it
+
+
+print(" ".join(results)) #simply print results
+
+#next will concatinate the strings with .join()
