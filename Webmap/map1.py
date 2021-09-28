e2093e84442ea145dabebd94e3dcf28618db3485
Andrei Clucerescu <adyac92@gmail.com>
2021-09-28 16:54:18 +0300

    WebMap: Population and more

--- Webmap/map1.py
+++ Webmap/map1.py
@@ -22,8 +22,12 @@ map = folium.Map(location=[38.58, -99.09],zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip (lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup = folium.Popup(str(el)+" m", 
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup = folium.Popup(str(el)+"m", 
     parse_html=True), fill_color = color_maker(el), color = 'grey', fill_opacity = 0.7 ))
+
+fg.add_child(folium.GeoJson(data=open('Webmap\\world.json', 'r', encoding='utf-8-sig').read(), 
+style_function=lambda x:{'fillColor':'green' if x['properties'] ['POP2005'] < 10000000 
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
 
