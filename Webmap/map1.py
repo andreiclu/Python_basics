fc7969e524d8b7dc080cf6a30c9963ed7ed219d2
Andrei Clucerescu <adyac92@gmail.com>
2021-09-28 16:16:51 +0300

    Changed Maps Detalis

--- Webmap/map1.py
+++ Webmap/map1.py
@@ -1,4 +1,5 @@
 
+from math import radians
 import folium
 
 import pandas
@@ -8,12 +9,21 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+def color_maker(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'   
+
 map = folium.Map(location=[38.58, -99.09],zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip (lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup = folium.Popup(str(el), parse_html=True), icon = folium.Icon(color='red')))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup = folium.Popup(str(el)+" m", 
+    parse_html=True), fill_color = color_maker(el), color = 'grey', fill_opacity = 0.7 ))
 
 map.add_child(fg)
 
