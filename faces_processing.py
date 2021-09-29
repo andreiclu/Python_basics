976bdb7ed750765fd1f71c3fd9a45ca3601ac250
Andrei Clucerescu <adyac92@gmail.com>
2021-09-29 17:18:19 +0300

    New Face

--- faces_processing.py
+++ faces_processing.py
@@ -2,7 +2,7 @@ import cv2
 
 face_cascade=cv2.CascadeClassifier("Files\\haarcascade_frontalface_default.xml")
 
-img=cv2.imread("Files\\Fata_mea.jpg")
+img=cv2.imread("Files\\photo.jpg")
 gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 faces=face_cascade.detectMultiScale(gray_img,
@@ -15,6 +15,8 @@ for x, y, w, h in faces:
 print(type(faces))
 print(faces)
 
-cv2.imshow("Gray", img)
+resized= cv2.resize(img,(int(img.shape[1]/3),int(img.shape[0]/3)))
+
+cv2.imshow("Gray", resized)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
